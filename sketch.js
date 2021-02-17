8a80585050f986f7a42502ba0ae2647b3c4e0d73
Patrick Marschke <patrick.m.marschke@gmail.com>
2021-02-17 14:06:26 -0600

    implemented modal move

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-let modal = document.getElementById("wikiModal");
-let wikiLink = document.getElementById("wikiLink");
-let modalClose = document.getElementsByClassName("close")[0];
+let modal = document.querySelector("#wikiModal");
+let wikiLink = document.querySelector("#wikiLink");
+let modalClose = document.querySelector(".close");
+let handle = document.querySelector(".handle");
 
-const entry = document.getElementById("entry-content");
+const entry = document.querySelector("#entry-content");
 let toggle = 0;
 
 // When the user clicks the button, open the modal
@@ -16,3 +17,44 @@ function wikiOpen(html) {
 modalClose.onclick = function () {
   modal.style.visibility = "hidden";
 };
+
+dragElement(modal);
+
+function dragElement(elmnt) {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  handle.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
